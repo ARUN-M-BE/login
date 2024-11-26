1e1dad0ce62c7711b97da8c38f187c59ca289958
Arun <arunm102301@gmail.com>
2024-11-26 20:57:46 +0530

    firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import SocialLogin from "./components/SocialLogin";
-import './App.css'
+import './App.css';
 import InputField from "./components/InputField";
 
 const App = () => {
---
+++ src/firebase/firebase.jsx
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1xW5yJcw3ZRVD0-bAI2Hxz-0mN0TgJLc",
+  authDomain: "login-arun.firebaseapp.com",
+  projectId: "login-arun",
+  storageBucket: "login-arun.firebasestorage.app",
+  messagingSenderId: "851950029612",
+  appId: "1:851950029612:web:82670d67a1e28dc6d17a89"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
