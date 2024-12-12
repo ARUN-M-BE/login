49fd26c9b4fe017ed882ee6d85c1840d8188d5ad
Arun <arunm102301@gmail.com>
2024-12-12 15:34:24 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,59 @@
+import React, { useState } from "react";
 import SocialLogin from "./components/SocialLogin";
 import './App.css';
 import InputField from "./components/InputField";
 
-const App = () => {
+const AuthPage = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const toggleAuthPage = () => setIsLogin(!isLogin);
+
   return (
-    <div className="login-container">
-      <h2 className="form-title">Log in with</h2>
-      
-      <form action="#" className="login-form">
-        <InputField type="email" placeholder="Email address" icon="mail" />
-        <InputField type="password" placeholder="Password" icon="lock" />
-
-        <a href="#" className="forgot-password-link">Forgot password?</a>
-        <button type="submit" className="login-button">Log In</button>
-      </form>
-
-      <p className="signup-prompt">
-        Don&apos;t have an account? <a href="#" className="signup-link">Sign up</a>
-      </p>
-
-      <p className="separator"><span>or</span></p>
-      <SocialLogin />
-
+    <div className="auth-container">
+      {isLogin ? (
+        <div className="login-container">
+          <h2 className="form-title">Log in with</h2>
+          <form action="#" className="login-form">
+            <InputField type="email" placeholder="Email address" icon="mail" />
+            <InputField type="password" placeholder="Password" icon="lock" />
+            <a href="#" className="forgot-password-link">Forgot password?</a>
+            <button type="submit" className="auth-button">Log In</button>
+          </form>
+          <p className="auth-toggle">
+            Don&apos;t have an account?{" "}
+            <a href="#" className="toggle-link" onClick={toggleAuthPage}>
+              Sign up
+            </a>
+          </p>
+          <p className="separator"><span>or</span></p>
+          <SocialLogin />
+        </div>
+      ) : (
+        <div className="signup-container">
+          <h2 className="form-title">Sign up with</h2>
+          <form action="#" className="signup-form">
+            <InputField type="text" placeholder="Full Name" icon="user" />
+            <InputField type="email" placeholder="Email address" icon="mail" />
+            <InputField type="password" placeholder="Password" icon="lock" />
+            <InputField
+              type="password"
+              placeholder="Confirm Password"
+              icon="lock"
+            />
+            <button type="submit" className="auth-button">Sign Up</button>
+          </form>
+          <p className="auth-toggle">
+            Already have an account?{" "}
+            <a href="#" className="toggle-link" onClick={toggleAuthPage}>
+              Log in
+            </a>
+          </p>
+          <p className="separator"><span>or</span></p>
+          <SocialLogin />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App;
+export default AuthPage;
--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -3,11 +3,12 @@ const SocialLogin = () => {
       <div className="social-login">
         <button className="social-button">
           <img src="google.svg" alt="Google" className="social-icon" />
-          Google
         </button>
         <button className="social-button">
           <img src="apple.svg" alt="Apple" className="social-icon" />
-          Apple
+        </button>
+        <button className="social-button">
+          <img src="Github.svg" alt="Github" className="social-icon" />
         </button>
       </div>
     )
