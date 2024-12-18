f365a4c9e1edbe9f5d2ed6f7d8bbe33816afa878
Arun <arunm102301@gmail.com>
2024-12-18 17:58:54 +0530

    get error this

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,34 @@ import React, { useState } from "react";
 import SocialLogin from "./components/SocialLogin";
 import './App.css';
 import InputField from "./components/InputField";
-import firebase from "./firebase/firebase";
+import firebase, { auth } from "./firebase/firebase";
+import { setDoc } from "firebase/firestore";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const AuthPage = () => {
   const [isLogin, setIsLogin] = useState(true);
 
   const toggleAuthPage = () => setIsLogin(!isLogin);
+  // const setIsRegister = async (e) =>{
+  //   await setDoc(doc(db,"users",res.user.uid),
+  //   {
+  //     username,
+  //     email,
+  //     password,
+  //     confirmpassword
+  //   });
+  //   setIsLogin(true);
+  // } 
+  const handleRegister = async (e) =>{
+    e.preventDefault();
+    try {
+      await createUserWithEmailAndPassword(auth,email,password,confirmpassword);
+      const user =auth.currentUser;
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className="auth-container">
@@ -32,14 +54,15 @@ const AuthPage = () => {
       ) : (
         <div className="signup-container">
           <h2 className="form-title">Sign up with</h2>
-          <form action="#" className="signup-form">
-            <InputField type="text" placeholder="Full Name" icon="user" />
-            <InputField type="email" placeholder="Email address" icon="mail" />
-            <InputField type="password" placeholder="Password" icon="lock" />
+          <form action="#" className="signup-form" onClick={handleRegister}>
+            <InputField type="text" placeholder="Full Name" icon="user" name="user"/>
+            <InputField type="email" placeholder="Email address" icon="mail" name="email" />
+            <InputField type="password" placeholder="Password" icon="lock" name="password" />
             <InputField
               type="password"
               placeholder="Confirm Password"
               icon="lock"
+              name="conformpassword"
             />
             <button type="submit" className="auth-button">Sign Up</button>
           </form>
